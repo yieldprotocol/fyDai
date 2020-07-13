b2a7f262e5da590e15d13ef4167973a8c7338eb6
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-13 23:24:35 +0100

    Fixed gas test

--- buidler.config.js
+++ buidler.config.js
@@ -13,30 +13,4 @@ module.exports = {
     gasReporter: {
         enabled: true
     },
-    networks: {
-        buidlerevm: {
-            accounts: [
-                {
-                    privateKey: "0xFFFE05032D46E7A2DB4FB9836C4D89120F1471BA838A456436673444DC84F440",
-                    balance: "0xFFFFFFFFFFFFFFFFFFFFFFFF"
-                },
-                {
-                    privateKey: "0xFFFE05032D46E7A2DB4FB9836C4D89120F1471BA838A456436673444DC84F441",
-                    balance: "0xFFFFFFFFFFFFFFFFFFFFFFFF"
-                },
-                {
-                    privateKey: "0xFFFE05032D46E7A2DB4FB9836C4D89120F1471BA838A456436673444DC84F442",
-                    balance: "0xFFFFFFFFFFFFFFFFFFFFFFFF"
-                },
-                {
-                    privateKey: "0xFFFE05032D46E7A2DB4FB9836C4D89120F1471BA838A456436673444DC84F443",
-                    balance: "0xFFFFFFFFFFFFFFFFFFFFFFFF"
-                },
-                {
-                    privateKey: "0xFFFE05032D46E7A2DB4FB9836C4D89120F1471BA838A456436673444DC84F444",
-                    balance: "0xFFFFFFFFFFFFFFFFFFFFFFFF"
-                },
-            ],
-        },
-    },
 };
