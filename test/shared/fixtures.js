9df1a9d6ecedc73e599ddb6b386e32b328a7529c
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-07-22 09:14:52 +0100

    Refactored fixtures

--- test/shared/fixtures.js
+++ test/shared/fixtures.js
@@ -12,22 +12,22 @@ const Controller = artifacts.require('Controller');
 
 const { WETH, Line, spotName, linel, limits, spot, rate1, chi1, toRay, subBN } = require("./utils");
 
-const setupYield = async(owner, user) => {
+const setupMaker = async() => {
     // Set up vat, join and weth
     vat = await Vat.new();
-    await vat.init(WETH, { from: owner }); // Set WETH rate to 1.0
+    await vat.init(WETH); // Set WETH rate to 1.0
 
-    weth = await Weth.new({ from: owner });
-    wethJoin = await GemJoin.new(vat.address, WETH, weth.address, { from: owner });
+    weth = await Weth.new();
+    wethJoin = await GemJoin.new(vat.address, WETH, weth.address);
 
-    dai = await ERC20.new(0, { from: owner });
-    daiJoin = await DaiJoin.new(vat.address, dai.address, { from: owner });
+    dai = await ERC20.new(0);
+    daiJoin = await DaiJoin.new(vat.address, dai.address);
 
     // Setup vat
-    await vat.file(WETH, spotName, spot, { from: owner });
-    await vat.file(WETH, linel, limits, { from: owner });
+    await vat.file(WETH, spotName, spot);
+    await vat.file(WETH, linel, limits);
     await vat.file(Line, limits); 
-    await vat.fold(WETH, vat.address, subBN(rate1, toRay(1)), { from: owner }); // Fold only the increase from 1.0
+    await vat.fold(WETH, vat.address, subBN(rate1, toRay(1))); // Fold only the increase from 1.0
 
     // Setup pot
     pot = await Pot.new(vat.address);
@@ -40,19 +40,18 @@ const setupYield = async(owner, user) => {
         dai.address,
     );
 
-    await pot.setChi(chi1, { from: owner });
+    await pot.setChi(chi1);
 
     // Setup jug
     jug = await Jug.new(vat.address);
-    await jug.init(WETH, { from: owner }); // Set WETH duty (stability fee) to 1.0
+    await jug.init(WETH); // Set WETH duty (stability fee) to 1.0
 
     // Permissions
-    await vat.rely(vat.address, { from: owner });
-    await vat.rely(wethJoin.address, { from: owner });
-    await vat.rely(daiJoin.address, { from: owner });
-    await vat.rely(pot.address, { from: owner });
-    await vat.rely(jug.address, { from: owner });
-    await vat.hope(daiJoin.address, { from: user });
+    await vat.rely(vat.address);
+    await vat.rely(wethJoin.address);
+    await vat.rely(daiJoin.address);
+    await vat.rely(pot.address);
+    await vat.rely(jug.address);
 
     treasury = await Treasury.new(
         vat.address,
@@ -63,7 +62,6 @@ const setupYield = async(owner, user) => {
         pot.address,
         chai.address,
     );
-    await treasury.orchestrate(owner);
 
     return {
         vat,
@@ -78,9 +76,22 @@ const setupYield = async(owner, user) => {
     }
 }
 
+// Helper for deploying Treasury
+async function newTreasury() {
+    return Treasury.new(
+        vat.address,
+        weth.address,
+        dai.address,
+        wethJoin.address,
+        daiJoin.address,
+        pot.address,
+        chai.address,
+    );
+}
+
 // Helper for deploying YDai
-async function newYdai(maturity, name, symbol) {
-    return YDai.new(
+async function newYDai(maturity, name, symbol) {
+    const yDai = await YDai.new(
         vat.address,
         jug.address,
         pot.address,
@@ -89,46 +100,43 @@ async function newYdai(maturity, name, symbol) {
         name,
         symbol,
     );
+    controller.addSeries(yDai.address);
+    yDai.orchestrate(controller.address);
+    treasury.orchestrate(yDai.address);
+    return yDai;
 }
 
 // Deploys the controller with 2 Ydai contracts with maturities at 1000 and 
 // 2000 blocks from now
-async function setupYdaiController(owner) {
+async function newController() {
     // Setup Controller
     controller = await Controller.new(
         vat.address,
         pot.address,
         treasury.address,
-        { from: owner },
     );
-    treasury.orchestrate(controller.address, { from: owner });
+    treasury.orchestrate(controller.address);
 
     // Setup yDai
-    const block = await web3.eth.getBlockNumber();
+    /* const block = await web3.eth.getBlockNumber();
     maturity1 = (await web3.eth.getBlock(block)).timestamp + 1000;
     yDai1 = await newYdai(maturity1, "Name1", "Symbol1")
-    controller.addSeries(yDai1.address, { from: owner });
-    yDai1.orchestrate(controller.address, { from: owner });
-    treasury.orchestrate(yDai1.address, { from: owner });
+    controller.addSeries(yDai1.address);
+    yDai1.orchestrate(controller.address);
+    treasury.orchestrate(yDai1.address);
 
     maturity2 = (await web3.eth.getBlock(block)).timestamp + 2000;
     yDai2 = await newYdai(maturity2, "Name2", "Symbol2")
-    controller.addSeries(yDai2.address, { from: owner });
-    yDai2.orchestrate(controller.address, { from: owner });
-    treasury.orchestrate(yDai2.address, { from: owner });
+    controller.addSeries(yDai2.address);
+    yDai2.orchestrate(controller.address);
+    treasury.orchestrate(yDai2.address);*/
 
-    return {
-        controller,
-        yDai1,
-        maturity1,
-        yDai2,
-        maturity2,
-
-    }
+    return controller;
 }
 
 module.exports = {
-    setupYield,
-    newYdai,
-    setupYdaiController,
+    setupMaker,
+    newTreasury,
+    newYDai,
+    newController,
 }
