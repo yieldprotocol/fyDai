7b6fc9418fa64b039e60ff847f45cb59ec8192ef
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-08-19 22:40:18 +0100

    Drafted `bySignature` methods

--- contracts/peripheral/LimitPool.sol
+++ contracts/peripheral/LimitPool.sol
@@ -27,7 +27,7 @@ contract LimitPool {
     /// @param daiIn Amount of dai being sold
     /// @param minYDaiOut Minimum amount of yDai being bought
     function sellDai(address to, uint128 daiIn, uint128 minYDaiOut)
-        external
+        public
         returns(uint256)
     {
         uint256 yDaiOut = pool.sellDai(msg.sender, to, daiIn);
@@ -38,12 +38,28 @@ contract LimitPool {
         return yDaiOut;
     }
 
+    /// @dev Sell Dai for yDai with an encoded signature for adding LimitPool as a delegate in Pool.
+    /// @param to Wallet receiving the yDai being bought
+    /// @param daiIn Amount of dai being sold
+    /// @param minYDaiOut Minimum amount of yDai being bought
+    /// @param deadline Latest block timestamp for which the signature is valid
+    /// @param v Signature parameter
+    /// @param r Signature parameter
+    /// @param s Signature parameter
+    function sellDaiBySignature(address to, uint128 daiIn, uint128 minYDaiOut, uint deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        returns(uint256)
+    {
+        pool.addDelegateBySignature(msg.sender, address(this), deadline, v, r, s);
+        return sellDai(to, daiIn, minYDaiOut);
+    }
+
     /// @dev Buy Dai for yDai
     /// @param to Wallet receiving the dai being bought
     /// @param daiOut Amount of dai being bought
     /// @param maxYDaiIn Maximum amount of yDai being sold
     function buyDai(address to, uint128 daiOut, uint128 maxYDaiIn)
-        external
+        public
         returns(uint256)
     {
         uint256 yDaiIn = pool.buyDai(msg.sender, to, daiOut);
@@ -54,12 +70,28 @@ contract LimitPool {
         return yDaiIn;
     }
 
+    /// @dev Buy Dai for yDai with an encoded signature for adding LimitPool as a delegate in Pool.
+    /// @param to Wallet receiving the dai being bought
+    /// @param daiOut Amount of dai being bought
+    /// @param maxYDaiIn Maximum amount of yDai being sold
+    /// @param deadline Latest block timestamp for which the signature is valid
+    /// @param v Signature parameter
+    /// @param r Signature parameter
+    /// @param s Signature parameter
+    function buyDaiBySignature(address to, uint128 daiOut, uint128 maxYDaiIn, uint deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        returns(uint256)
+    {
+        pool.addDelegateBySignature(msg.sender, address(this), deadline, v, r, s);
+        return buyDai(to, daiOut, maxYDaiIn);
+    }
+
     /// @dev Sell yDai for Dai
     /// @param to Wallet receiving the dai being bought
     /// @param yDaiIn Amount of yDai being sold
     /// @param minDaiOut Minimum amount of dai being bought
     function sellYDai(address to, uint128 yDaiIn, uint128 minDaiOut)
-        external
+        public
         returns(uint256)
     {
         uint256 daiOut = pool.sellYDai(msg.sender, to, yDaiIn);
@@ -70,12 +102,28 @@ contract LimitPool {
         return daiOut;
     }
 
+    /// @dev Sell yDai for Dai with an encoded signature for adding LimitPool as a delegate in Pool.
+    /// @param to Wallet receiving the dai being bought
+    /// @param yDaiIn Amount of yDai being sold
+    /// @param minDaiOut Minimum amount of dai being bought
+    /// @param deadline Latest block timestamp for which the signature is valid
+    /// @param v Signature parameter
+    /// @param r Signature parameter
+    /// @param s Signature parameter
+    function sellYDaiBySignature(address to, uint128 yDaiIn, uint128 minDaiOut, uint deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        returns(uint256)
+    {
+        pool.addDelegateBySignature(msg.sender, address(this), deadline, v, r, s);
+        return sellYDai(to, yDaiIn, minDaiOut);
+    }
+
     /// @dev Buy yDai for dai
     /// @param to Wallet receiving the yDai being bought
     /// @param yDaiOut Amount of yDai being bought
     /// @param maxDaiIn Maximum amount of dai being sold
     function buyYDai(address to, uint128 yDaiOut, uint128 maxDaiIn)
-        external
+        public
         returns(uint256)
     {
         uint256 daiIn = pool.buyYDai(msg.sender, to, yDaiOut);
@@ -84,5 +132,21 @@ contract LimitPool {
             "LimitPool: Limit exceeded"
         );
         return daiIn;
+    }
+
+    /// @dev Buy yDai for dai with an encoded signature for adding LimitPool as a delegate in Pool.
+    /// @param to Wallet receiving the yDai being bought
+    /// @param yDaiOut Amount of yDai being bought
+    /// @param maxDaiIn Maximum amount of dai being sold
+    /// @param deadline Latest block timestamp for which the signature is valid
+    /// @param v Signature parameter
+    /// @param r Signature parameter
+    /// @param s Signature parameter
+    function buyYDaiBySignature(address to, uint128 yDaiOut, uint128 maxDaiIn, uint deadline, uint8 v, bytes32 r, bytes32 s)
+        public
+        returns(uint256)
+    {
+        pool.addDelegateBySignature(msg.sender, address(this), deadline, v, r, s);
+        return buyYDai(to, yDaiOut, maxDaiIn);
     }
 }
