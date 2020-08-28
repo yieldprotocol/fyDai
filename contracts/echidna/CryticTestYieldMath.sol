85786ac2deaa43a3638f4a2aa4b864d6031f2125
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-08-28 18:39:53 +0100

    Echidna and regular tests set

--- contracts/echidna/CryticTestYieldMath.sol
+++ contracts/echidna/CryticTestYieldMath.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.6.10;
-import "../pool/YieldMath.sol";
+import "../mocks/YieldMath128.sol";
 import "../pool/ABDKMath64x64.sol";
 
 
 contract CryticTestYieldMath {
-    uint128 internal oneDAI = 10**18;
-    uint128 internal tol = oneDAI;
+    // uint128 constant internal precision = 10**27;
     int128 constant internal k = int128(uint256((1 << 64)) / 126144000); // 1 / Seconds in 4 years, in 64.64
     int128 constant internal g = int128(uint256((999 << 64)) / 1000); // All constants are `ufixed`, to divide them they must be converted to uint256
 
@@ -17,13 +16,10 @@ contract CryticTestYieldMath {
     uint128 maxDaiReserves = 10**27; // $1B
     uint128 maxYDaiReserves = 10**27; // $1B
     uint128 maxTrade = 10**26; // $100M
-    uint128 maxTimeTillMaturity = uint128(k);
+    uint128 maxTimeTillMaturity = 31556952;
 
     constructor() public {}
     
-    function greaterWithTol(uint256 x, uint256 y) internal view returns (bool) {
-        return (x > y + tol); 
-    }
     /// @dev Bali Overflow-protected addition, from OpenZeppelin
     function add(uint128 a, uint128 b)
         internal pure returns (uint128)
@@ -39,16 +35,16 @@ contract CryticTestYieldMath {
         return c;
     }
 
-    function testLiquidityInvariant(uint128 daiReserves, uint128 yDAIReserves, uint128 daiIn, uint128 timeTillMaturity) public view {
+    function testLiquidityInvariant(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiIn, uint128 timeTillMaturity) public view returns (bool){
         daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
         yDAIReserves = minYDaiReserves + yDAIReserves % maxYDaiReserves;
-        daiIn = minTrade + daiIn % maxTrade;
         timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
 
-        uint256 reservesBefore = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
-        uint128 yDaiOut = YieldMath.yDaiOutForDaiIn(daiReserves, yDAIReserves, daiIn, timeTillMaturity, k, g);
-        uint256 reservesAfter = initialReservesValue(add(daiReserves, daiIn), sub(yDAIReserves, yDaiOut), add(timeTillMaturity, 1));
-        assert(greaterWithTol(reservesAfter, reservesBefore));
+        uint128 reserves_0 = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
+        uint128 daiOut = YieldMath128.daiOutForYDaiIn(daiReserves, yDAIReserves, yDaiIn, timeTillMaturity, k, g);
+        uint128 reserves_1 = initialReservesValue(sub(daiReserves, daiOut), add(yDAIReserves, yDaiIn), sub(timeTillMaturity, 1));
+        assert(reserves_0 < (reserves_1/* + precision*/));
+        return reserves_0 < (reserves_1/* + precision*/);
     }
 
     /**
@@ -61,17 +57,17 @@ contract CryticTestYieldMath {
      */
     function initialReservesValue (
         uint128 daiReserves, uint128 yDAIReserves, uint128 timeTillMaturity)
-    internal pure returns (uint128) {
+    public pure returns (uint128) {
         // a = (1 - k * timeTillMaturity)
         int128 a = ABDKMath64x64.sub (0x10000000000000000, ABDKMath64x64.mul (k, ABDKMath64x64.fromUInt (timeTillMaturity)));
         require (a > 0);
 
         uint256 sum =
-        uint256 (YieldMath.pow (daiReserves, uint128 (a), 0x10000000000000000)) +
-        uint256 (YieldMath.pow (yDAIReserves, uint128 (a), 0x10000000000000000)) >> 1;
+        uint256 (YieldMath128.pow (daiReserves, uint128 (a), 0x10000000000000000)) +
+        uint256 (YieldMath128.pow (yDAIReserves, uint128 (a), 0x10000000000000000)) >> 1;
         require (sum < 0x100000000000000000000000000000000);
 
-        uint256 result = uint256 (YieldMath.pow (uint128 (sum), 0x10000000000000000, uint128 (a))) << 1;
+        uint256 result = uint256 (YieldMath128.pow (uint128 (sum), 0x10000000000000000, uint128 (a))) << 1;
         require (result < 0x100000000000000000000000000000000);
 
         return uint128 (result);
--- test/echidna/601_liquidity_invariant.ts
+++ test/echidna/601_liquidity_invariant.ts
@@ -1,7 +1,9 @@
 const Crytic = artifacts.require('CryticTestYieldMath')
+const YieldMath = artifacts.require('YieldMathMock')
 
 // @ts-ignore
 import helper from 'ganache-time-traveler'
+import { bnify } from '../shared/utils'
 import { Contract } from '../shared/fixtures'
 // @ts-ignore
 
@@ -10,6 +12,11 @@ contract('YieldMath', async (accounts) => {
   let snapshotId: string
 
   let test: Contract
+  let yieldMath: Contract
+
+  const b = bnify('18446744073709551615')
+  const k = bnify('126144000').div(b)
+  const g = bnify('999').mul(b).div(1000)
 
   beforeEach(async () => {
     snapshot = await helper.takeSnapshot()
@@ -17,9 +24,47 @@ contract('YieldMath', async (accounts) => {
 
     // Setup YieldMathDAIWrapper
     test = await Crytic.new()
+    yieldMath = await YieldMath.new()
   })
 
-  it('', async () => {
-    await test.testLiquidityInvariant();
+  it('Outputs the invariant for two consecutive seconds', async () => {
+    // maxDaiReserves = 10**27; // $1B
+    // maxYDaiReserves = 10**27; // $1B
+    // maxTrade = 10**26; // $100M
+    // maxTimeTillMaturity = 31556952;
+
+    // const daiReserves = '66329041300990984000'
+    // const yDaiReserves = '34400000000000000000'
+    // const yDaiIn = '1000000000000000000'
+    // const timeTillMaturity = '31556951'
+
+    const minDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    const minYDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    const minTrade = '1000000000000000000' // 10**18; // $1
+    const minTimeTillMaturity = 1;
+
+    const daiReserves = minDaiReserves
+    const yDaiReserves = minYDaiReserves
+    const yDaiIn = minTrade
+    const timeTillMaturity = minTimeTillMaturity
+
+    console.log('yDai Reserves:       ' + yDaiReserves.toString())
+    console.log('Dai Reserves:        ' + daiReserves.toString())
+    console.log('Time until maturity: ' + timeTillMaturity.toString())
+    
+    console.log('yDai in:             ' + yDaiIn.toString())
+    console.log('Reserves value:      ' + (await test.initialReservesValue(daiReserves, yDaiReserves, timeTillMaturity)).toString());
+    const daiOut = bnify(await yieldMath.daiOutForYDaiIn128(daiReserves, yDaiReserves, yDaiIn, timeTillMaturity, k, g));
+    console.log('Dai out:             ' + daiOut.toString())
+    console.log('Reserves value:      ' + 
+      bnify(
+        await test.initialReservesValue(
+          bnify(daiReserves).sub(daiOut).toString(),
+          bnify(yDaiReserves).add(yDaiIn).toString(),
+          bnify(timeTillMaturity).sub(1).toString(),
+        )
+      ).toString()
+    )
+    // console.log((await test.testLiquidityInvariant('66329041300990984000', '34400000000000000000', '10000000000000000000', '31556951')).toString());
   })
 })
