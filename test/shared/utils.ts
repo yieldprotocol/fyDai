7bd2a1b9b4945f9374809c59e8052ccc3cf686b1
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-08-28 10:43:42 +0100

    Realistic values as default for all Maker parameters

--- test/shared/utils.ts
+++ test/shared/utils.ts
@@ -86,9 +86,9 @@ const UNIT: BigNumber = BigNumber.from(10).pow(BigNumber.from(27))
 
 export const limits = toRad(10000)
 
-export const spot = toRay(150) // At the time of writing, spot is 258.05, a reasonable range is [50, 1000]
-export const chi1 = toRay(1.1) // At the time of writing, chi is 1.018008449363110619399951035, a reasonable upper boundary is 1.2
-export const rate1 = toRay(1.02) // At the time of writing, rate is 1.02004188, a reasonable upper boundary is 1.2
+export const spot = toRay(260) // At the time of writing, spot is 259.625048333333333333333333333, a reasonable range is [50, 1000]
+export const chi1 = toRay(1.01) // At the time of writing, chi is 1.018008449363110619399951035, a reasonable upper boundary is 1.2
+export const rate1 = toRay(1.02) // At the time of writing, rate is 1.020041883692153436559184034, a reasonable upper boundary is 1.2
 export const precision = 10 // Loss in wei that is tolerated with each operation
 
 export const daiDebt1 = toWad(120).toString()
