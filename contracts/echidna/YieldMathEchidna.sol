0f4ba92bcd2441af734eec621025524dff99e4aa
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-08-31 19:26:38 +0100

    Fuzzing tests work for reverse trade invariant

--- contracts/echidna/YieldMathEchidna.sol
+++ contracts/echidna/YieldMathEchidna.sol
@@ -8,18 +8,18 @@ import "@nomiclabs/buidler/console.sol";
 
 
 contract YieldMathEchidna {
-    // uint128 constant internal precision = 10**27;
+    uint128 constant internal precision = 1e12;
     int128 constant internal k = int128(uint256((1 << 64)) / 126144000); // 1 / Seconds in 4 years, in 64.64
-    int128 constant internal g = int128(uint256((999 << 64)) / 1000); // All constants are `ufixed`, to divide them they must be converted to uint256
+    int128 constant internal g = int128(uint256((95 << 64)) / 100); // All constants are `ufixed`, to divide them they must be converted to uint256
 
     uint128 minDaiReserves = 10**21; // $1000
     uint128 minYDaiReserves = minDaiReserves + 1;
     uint128 minTrade = minDaiReserves / 1000; // $1
-    uint128 minTimeTillMaturity = 43200;
+    uint128 minTimeTillMaturity = 0;
     uint128 maxDaiReserves = 10**27; // $1B
     uint128 maxYDaiReserves = maxDaiReserves + 1; // $1B
     uint128 maxTrade = maxDaiReserves / 10;
-    uint128 maxTimeTillMaturity = 31556952;
+    uint128 maxTimeTillMaturity = 126144000;
 
     constructor() public {}
     
@@ -38,6 +38,47 @@ contract YieldMathEchidna {
         return c;
     }
 
+    function testSellYDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiIn, uint128 timeTillMaturity) public view returns (bool){
+        require(daiReserves > yDAIReserves);
+        daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
+        yDAIReserves = minYDaiReserves + yDAIReserves % maxYDaiReserves;
+        timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
+
+        uint128 yDaiOut = sellYDaiAndReverse(daiReserves, yDAIReserves, yDaiIn, timeTillMaturity);
+        assert(yDaiOut <= add(yDaiIn, precision));
+    }
+
+    function testBuyYDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiOut, uint128 timeTillMaturity) public view returns (bool){
+        require(daiReserves > yDAIReserves);
+        daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
+        yDAIReserves = minYDaiReserves + yDAIReserves % maxYDaiReserves;
+        timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
+
+        uint128 yDaiIn = buyYDaiAndReverse(daiReserves, yDAIReserves, yDaiOut, timeTillMaturity);
+        assert(sub(yDaiOut, precision) <= yDaiIn);
+    }
+
+    function testSellDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 daiIn, uint128 timeTillMaturity) public view returns (bool){
+        require(daiReserves > yDAIReserves);
+        daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
+        yDAIReserves = minYDaiReserves + yDAIReserves % maxYDaiReserves;
+        timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
+
+        uint128 daiOut = sellDaiAndReverse(daiReserves, yDAIReserves, daiIn, timeTillMaturity);
+        assert(daiOut <= add(daiIn, precision));
+    }
+
+    function testBuyDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 daiOut, uint128 timeTillMaturity) public view returns (bool){
+        require(daiReserves > yDAIReserves);
+        daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
+        yDAIReserves = minYDaiReserves + yDAIReserves % maxYDaiReserves;
+        timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
+
+        uint128 daiIn = buyYDaiAndReverse(daiReserves, yDAIReserves, daiOut, timeTillMaturity);
+        assert(sub(daiOut, precision) <= daiIn);
+    }
+
+    /*
     function testLiquidityDaiOutForYDaiIn(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiIn, uint128 timeTillMaturity) public view returns (bool){
         if (daiReserves > yDAIReserves) return true;
         daiReserves = minDaiReserves + daiReserves % maxDaiReserves;
@@ -45,11 +86,12 @@ contract YieldMathEchidna {
         timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
 
         uint128 reserves_0 = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
-        uint128 daiOut= YieldMath128.daiOutForYDaiIn(daiReserves, yDAIReserves, yDaiIn, timeTillMaturity, k, g);
+        uint128 daiOut= YieldMath64.daiOutForYDaiIn(daiReserves, yDAIReserves, yDaiIn, timeTillMaturity, k, g);
         uint128 reserves_1 = initialReservesValue(sub(daiReserves, daiOut), add(yDAIReserves, yDaiIn), sub(timeTillMaturity, 1));
         assert(reserves_0 < (reserves_1)); // + precision));
         return reserves_0 < (reserves_1); // + precision);
     }
+    */
 
     /*
     function testLiquidityDaiInForYDaiOut(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiOut, uint128 timeTillMaturity) public view returns (bool){
@@ -59,7 +101,7 @@ contract YieldMathEchidna {
         timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
 
         uint128 reserves_0 = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
-        uint128 daiIn= YieldMath128.daiInForYDaiOut(daiReserves, yDAIReserves, yDaiOut, timeTillMaturity, k, g);
+        uint128 daiIn= YieldMath64.daiInForYDaiOut(daiReserves, yDAIReserves, yDaiOut, timeTillMaturity, k, g);
         uint128 reserves_1 = initialReservesValue(add(daiReserves, daiIn), sub(yDAIReserves, yDaiOut), sub(timeTillMaturity, 1));
         assert(reserves_0 < (reserves_1)); // + precision));
         return reserves_0 < (reserves_1); // + precision);
@@ -72,7 +114,7 @@ contract YieldMathEchidna {
         timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
 
         uint128 reserves_0 = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
-        uint128 yDaiOut= YieldMath128.yDaiOutForDaiIn(daiReserves, yDAIReserves, daiIn, timeTillMaturity, k, g);
+        uint128 yDaiOut= YieldMath64.yDaiOutForDaiIn(daiReserves, yDAIReserves, daiIn, timeTillMaturity, k, g);
         uint128 reserves_1 = initialReservesValue(add(daiReserves, daiIn), sub(yDAIReserves, yDaiOut), sub(timeTillMaturity, 1));
         assert(reserves_0 < (reserves_1)); // + precision));
         return reserves_0 < (reserves_1); // + precision);
@@ -85,7 +127,7 @@ contract YieldMathEchidna {
         timeTillMaturity = minTimeTillMaturity + timeTillMaturity % maxTimeTillMaturity;
 
         uint128 reserves_0 = initialReservesValue(daiReserves, yDAIReserves, timeTillMaturity);
-        uint128 yDaiIn= YieldMath128.yDaiInForDaiOut(daiReserves, yDAIReserves, daiOut, timeTillMaturity, k, g);
+        uint128 yDaiIn= YieldMath64.yDaiInForDaiOut(daiReserves, yDAIReserves, daiOut, timeTillMaturity, k, g);
         uint128 reserves_1 = initialReservesValue(sub(daiReserves, daiOut), add(yDAIReserves, yDaiIn), sub(timeTillMaturity, 1));
         assert(reserves_0 < (reserves_1)); // + precision));
         return reserves_0 < (reserves_1); // + precision);
@@ -94,20 +136,20 @@ contract YieldMathEchidna {
 
     /*
     function testLog2MonotonicallyGrows(uint128 x) public view {
-        uint128 z1= YieldMath128.log_2(x);
-        uint128 z2= YieldMath128.log_2(x + 1);
+        uint128 z1= YieldMath64.log_2(x);
+        uint128 z2= YieldMath64.log_2(x + 1);
         assert(z2 >= z1);
     }
 
     function testLog2PrecisionLossRoundsDown(uint128 x) public view {
-        uint128 z1 = YieldMath.log_2(x);
-        uint128 z2= YieldMath128.log_2(x);
-        assert(z2 >= z1);
+        uint128 z1 = YieldMath64.log_2(x);
+        uint128 z2= YieldMath64.log_2(x);
+        assert(z2 >= z1);portugal
     }
 
     function testPow2PrecisionLossRoundsDown(uint128 x) public view {
-        uint128 z1 = YieldMath.pow_2(x);
-        uint128 z2= YieldMath128.pow_2(x);
+        uint128 z1 = YieldMath64.pow_2(x);
+        uint128 z2= YieldMath64.pow_2(x);
         assert(z2 >= z1);
     } */
 
@@ -119,7 +161,7 @@ contract YieldMathEchidna {
      * @param timeTillMaturity time till maturity in seconds
      * @return estimated value of reserves
      */
-    function initialReservesValue (
+    /* function initialReservesValue (
         uint128 daiReserves, uint128 yDAIReserves, uint128 timeTillMaturity)
         public pure returns (uint128)
     {
@@ -128,13 +170,45 @@ contract YieldMathEchidna {
         require (a > 0);
 
         uint256 sum =
-        uint256 (YieldMath128.pow (daiReserves, uint128 (a), 0x10000000000000000)) +
-        uint256 (YieldMath128.pow (yDAIReserves, uint128 (a), 0x10000000000000000)) >> 1;
+        uint256 (YieldMath64.pow (daiReserves, uint128 (a), 0x10000000000000000)) +
+        uint256 (YieldMath64.pow (yDAIReserves, uint128 (a), 0x10000000000000000)) >> 1;
         require (sum < 0x100000000000000000000000000000000);
 
-        uint256 result = uint256 (YieldMath128.pow (uint128 (sum), 0x10000000000000000, uint128 (a))) << 1;
+        uint256 result = uint256 (YieldMath64.pow (uint128 (sum), 0x10000000000000000, uint128 (a))) << 1;
         require (result < 0x100000000000000000000000000000000);
 
         return uint128 (result);
+    } */
+
+    /// @dev Sell yDai and sell the obtained Dai back for yDai
+    function sellYDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiIn, uint128 timeTillMaturity)
+        internal pure returns (uint128)
+    {
+        uint128 daiAmount = YieldMath64.daiOutForYDaiIn(daiReserves, yDAIReserves, yDaiIn, timeTillMaturity, k, g);
+        return YieldMath64.yDaiOutForDaiIn(sub(daiReserves, daiAmount), add(yDAIReserves, yDaiIn), daiAmount, timeTillMaturity, k, g);
+    }
+
+    /// @dev Buy yDai and sell it back
+    function buyYDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 yDaiOut, uint128 timeTillMaturity)
+        internal pure returns (uint128)
+    {
+        uint128 daiAmount = YieldMath64.daiInForYDaiOut(daiReserves, yDAIReserves, yDaiOut, timeTillMaturity, k, g);
+        return YieldMath64.yDaiInForDaiOut(add(daiReserves, daiAmount), sub(yDAIReserves, yDaiOut), daiAmount, timeTillMaturity, k, g);
+    }
+
+    /// @dev Sell yDai and sell the obtained Dai back for yDai
+    function sellDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 daiIn, uint128 timeTillMaturity)
+        internal pure returns (uint128)
+    {
+        uint128 yDaiAmount = YieldMath64.yDaiOutForDaiIn(daiReserves, yDAIReserves, daiIn, timeTillMaturity, k, g);
+        return YieldMath64.daiOutForYDaiIn(add(daiReserves, daiIn), sub(yDAIReserves, yDaiAmount), yDaiAmount, timeTillMaturity, k, g);
+    }
+
+    /// @dev Buy yDai and sell it back
+    function buyDaiAndReverse(uint128 daiReserves, uint128 yDAIReserves, uint128 daiOut, uint128 timeTillMaturity)
+        internal pure returns (uint128)
+    {
+        uint128 yDaiAmount = YieldMath64.yDaiInForDaiOut(daiReserves, yDAIReserves, daiOut, timeTillMaturity, k, g);
+        return YieldMath64.daiInForYDaiOut(add(daiReserves, daiOut), sub(yDAIReserves, yDaiAmount), yDaiAmount, timeTillMaturity, k, g);
     }
 }
--- test/echidna/601_liquidity_invariant.ts
+++ test/echidna/601_liquidity_invariant.ts
@@ -37,7 +37,7 @@ contract('YieldMath', async (accounts) => {
     console.log('    ' + (await yieldMath.log_2_128('4000916067400943041987521580724335132')).toString())
   })
 
-  it.only('Outputs the invariant for two consecutive seconds', async () => {
+  it('Outputs the invariant for two consecutive seconds', async () => {
     // maxDaiReserves = 10**27; // $1B
     // maxYDaiReserves = 10**27; // $1B
     // maxTrade = 10**26; // $100M
@@ -53,10 +53,10 @@ contract('YieldMath', async (accounts) => {
     // const yDaiIn = minTrade
     // const timeTillMaturity = minTimeTillMaturity
 
-    const daiReserves = '1000000000000000000000'
-    const yDaiReserves = '1000000000000000000000'
-    const yDaiIn = '1000000000000000000'
-    const timeTillMaturity = '43204'
+    const daiReserves =   '998999999999999999998'
+    const yDaiReserves = '1001000000000000000001'
+    const yDaiIn =          '1000000000000000000'
+    const timeTillMaturity = '43199'
 
     console.log('yDai Reserves:       ' + yDaiReserves.toString())
     console.log('Dai Reserves:        ' + daiReserves.toString())
@@ -75,5 +75,117 @@ contract('YieldMath', async (accounts) => {
       ).toString()
     )
     // console.log((await test.testLiquidityInvariant('66329041300990984000', '34400000000000000000', '10000000000000000000', '31556951')).toString());
+  })
+
+  it('Sells yDai and reverses the trade', async () => {
+    // maxDaiReserves = 10**27; // $1B
+    // maxYDaiReserves = 10**27; // $1B
+    // maxTrade = 10**26; // $100M
+    // maxTimeTillMaturity = 31556952;
+
+    // const minDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minYDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minTrade = '1000000000000000000' // 10**18; // $1
+    // const minTimeTillMaturity = 1;
+
+    // const daiReserves = minDaiReserves
+    // const yDaiReserves = minYDaiReserves
+    // const yDaiIn = minTrade
+    // const timeTillMaturity = minTimeTillMaturity
+
+    const daiReserves =   '1000000000000000000000'
+    const yDaiReserves = '1000000000000000000001'
+    const yDaiIn =          '1000000000000000000'
+    const timeTillMaturity = '43200'
+
+    console.log('yDai Reserves:       ' + yDaiReserves.toString())
+    console.log('Dai Reserves:        ' + daiReserves.toString())
+    console.log('Time until maturity: ' + timeTillMaturity.toString())
+    console.log('yDai in:             ' + yDaiIn.toString())
+    console.log('yDai out:            ' + (await test.sellYDaiAndReverse(daiReserves, yDaiReserves, yDaiIn, timeTillMaturity)).toString());
+  })
+
+  it('Buys yDai and reverses the trade', async () => {
+    // maxDaiReserves = 10**27; // $1B
+    // maxYDaiReserves = 10**27; // $1B
+    // maxTrade = 10**26; // $100M
+    // maxTimeTillMaturity = 31556952;
+
+    // const minDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minYDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minTrade = '1000000000000000000' // 10**18; // $1
+    // const minTimeTillMaturity = 1;
+
+    // const daiReserves = minDaiReserves
+    // const yDaiReserves = minYDaiReserves
+    // const yDaiIn = minTrade
+    // const timeTillMaturity = minTimeTillMaturity
+
+    const daiReserves =   '349061773210894792196710'
+    const yDaiReserves = '1001649248511020033788'
+    const yDaiOut =          '1000000000000000001'
+    const timeTillMaturity = '49034'
+
+    console.log('yDai Reserves:       ' + yDaiReserves.toString())
+    console.log('Dai Reserves:        ' + daiReserves.toString())
+    console.log('Time until maturity: ' + timeTillMaturity.toString())
+    console.log('yDai out:             ' + yDaiOut.toString())
+    console.log('yDai in:            ' + (await test.buyYDaiAndReverse(daiReserves, yDaiReserves, yDaiOut, timeTillMaturity)).toString());
+  })
+
+  it('Sells Dai and reverses the trade', async () => {
+    // maxDaiReserves = 10**27; // $1B
+    // maxYDaiReserves = 10**27; // $1B
+    // maxTrade = 10**26; // $100M
+    // maxTimeTillMaturity = 31556952;
+
+    // const minDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minYDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minTrade = '1000000000000000000' // 10**18; // $1
+    // const minTimeTillMaturity = 1;
+
+    // const daiReserves = minDaiReserves
+    // const yDaiReserves = minYDaiReserves
+    // const yDaiIn = minTrade
+    // const timeTillMaturity = minTimeTillMaturity
+
+    const daiReserves =   '349061773210894792196710'
+    const yDaiReserves = '1001649248511020033788'
+    const daiIn =          '1000000000000000001'
+    const timeTillMaturity = '49034'
+
+    console.log('yDai Reserves:       ' + yDaiReserves.toString())
+    console.log('Dai Reserves:        ' + daiReserves.toString())
+    console.log('Time until maturity: ' + timeTillMaturity.toString())
+    console.log('Dai in:             ' + daiIn.toString())
+    console.log('Dai out:            ' + (await test.sellDaiAndReverse(daiReserves, yDaiReserves, daiIn, timeTillMaturity)).toString());
+  })
+
+  it('Buys Dai and reverses the trade', async () => {
+    // maxDaiReserves = 10**27; // $1B
+    // maxYDaiReserves = 10**27; // $1B
+    // maxTrade = 10**26; // $100M
+    // maxTimeTillMaturity = 31556952;
+
+    // const minDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minYDaiReserves = '1000000000000000000000' // 10**21; // $1000
+    // const minTrade = '1000000000000000000' // 10**18; // $1
+    // const minTimeTillMaturity = 1;
+
+    // const daiReserves = minDaiReserves
+    // const yDaiReserves = minYDaiReserves
+    // const yDaiIn = minTrade
+    // const timeTillMaturity = minTimeTillMaturity
+
+    const daiReserves =   '925173878424482809107308121'
+    const yDaiReserves = '623579633693919687679844805'
+    const daiOut =          '1001078616452680644'
+    const timeTillMaturity = '33'
+
+    console.log('yDai Reserves:       ' + yDaiReserves.toString())
+    console.log('Dai Reserves:        ' + daiReserves.toString())
+    console.log('Time until maturity: ' + timeTillMaturity.toString())
+    console.log('Dai out:             ' + daiOut.toString())
+    console.log('Dai in:            ' + (await test.buyDaiAndReverse(daiReserves, yDaiReserves, daiOut, timeTillMaturity)).toString());
   })
 })
