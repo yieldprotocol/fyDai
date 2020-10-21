743175c35299ff435701413afcff35d2c673094c
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-10-21 09:15:00 +0100

    fix: don't count fyDai

--- scripts/pool_liquidity.js
+++ scripts/pool_liquidity.js
@@ -26,17 +26,15 @@ module.exports = async (deployer, network) => {
   treasury = await Treasury.at(await migrations.contracts(web3.utils.fromAscii('Treasury')))
   vat = await Vat.at(await migrations.contracts(web3.utils.fromAscii('Vat')))
 
-  console.log('Treasury')
-  const savings = await treasury.savings()
-  const debt = await treasury.debt()
-  console.log(`Dai Reserves:   ${savings.div(WAD)}`)
-  console.log(`Dai Debt: ${debt.div(WAD)}`)
-
   const eth = (await vat.urns(web3.utils.fromAscii('ETH-A'), treasury.address)).ink
   const spot = (await vat.ilks(web3.utils.fromAscii('ETH-A'))).spot
   const collateral = mulRay(eth, spot)
-  console.log(`Eth Collateral: ${collateral.div(WAD)} (${eth.div(WAD)} Eth)`)
-  console.log()
+  console.log(`Treasury Eth:   ${collateral.div(WAD)} (${eth.div(WAD)} Eth)`)
+
+  const savings = await treasury.savings()
+  const debt = await treasury.debt()
+  console.log(`Treasury Dai:   ${savings.div(WAD)}`)
+  console.log(`Treasury Debt:  ${debt.div(WAD)}`)
 
   let tvl = savings.add(collateral).sub(debt)
   const contracts = await migrations.length()
@@ -45,16 +43,11 @@ module.exports = async (deployer, network) => {
 
     if (contractName.includes('fyDaiLP')) {
       const pool = await Pool.at(await migrations.contracts(web3.utils.fromAscii(contractName)))
-      console.log(contractName)
       const dai = await pool.getDaiReserves()
-      const fyDai = await pool.getFYDaiReserves()
-      console.log(`Dai Reserves:   ${dai.div(WAD)}`)
-      console.log(`FYDai Reserves: ${fyDai.div(WAD)}`)
-      console.log()
-      tvl = tvl.add(dai).add(fyDai)
+      console.log(`${contractName}:   ${dai.div(WAD)}`)
+      tvl = tvl.add(dai)
     }
   }
-  console.log()
-  console.log(`TVL: ${tvl.div(WAD)}`)
+  console.log(`TVL:            ${tvl.div(WAD)}`)
   console.log('Press Ctrl+C to exit')
 }
